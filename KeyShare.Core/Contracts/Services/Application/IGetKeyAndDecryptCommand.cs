bcc90ab4202fe30ca9b9b714fd638fcc25c5eb54
Konstantin Rasputin <77513299+rasputyashka@users.noreply.github.com>
2025-05-18 04:07:29 +0300

    Update IGetKeyAndDecryptCommand.cs

--- KeyShare.Core/Contracts/Services/Application/IGetKeyAndDecryptCommand.cs
+++ KeyShare.Core/Contracts/Services/Application/IGetKeyAndDecryptCommand.cs
@@ -1,6 +1,5 @@
 ﻿namespace KeyShare.Core.Contracts.Services.Application;
 public interface IGetKeyAndDecryptCommand
 {
-    // actually CipherText object is better here
     public string Execute(byte[] cipherText, byte[] iv,  int keyID);
 }
